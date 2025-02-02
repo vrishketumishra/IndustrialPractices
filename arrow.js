3e559873e9b996db87c8af5faa363fe36a03393b
vrishketumishra <rishimishra2006@gmail.com>
2025-02-02 10:25:07 +0530

    Added projrct files

---
+++ arrow.js
@@ -0,0 +1,10 @@
+const greet = (name) => {
+    console.log(`hello ${name}`);
+};
+console.log(greet("john"));
+
+//odd number
+const fliterOdds =(arr) => arr.filter(num = num52 !== 0);
+const nums = [1,2,3,4,5,6]
+console.log(filterOdds(nums))
+</body>
